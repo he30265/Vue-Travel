358505984de77e5be0fa22737300d13a3b1c7258
刘政和 <liuzhenghe@192.168.0.110>
2019-05-26 19:09:43 +0800

    city-router

---
+++ src/pages/city/components/header.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="header">
+    <router-link to="/">
+      <div class="header_back">
+        <span class="iconfont">&#xe624;</span>
+      </div>
+    </router-link>
+
+    <div class="header_txt">城市选择</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CityHeader"
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '~style/varibles';
+
+.header {
+  position: relative;
+  background-color: $bgColor;
+  overflow: hidden;
+  color: #fff;
+  padding: 0.2rem;
+
+  .header_back {
+    color: #fff;
+    position: absolute;
+  }
+
+  .header_txt {
+    text-align: center;
+  }
+}
+</style>
--- src/pages/home/components/header.vue
+++ src/pages/home/components/header.vue
@@ -1,47 +1,53 @@
 <template>
-<div class="header">
+  <div class="header">
     <div class="header_left">
-        <span class="iconfont">&#xe624;</span>
+      <span class="iconfont">&#xe624;</span>
     </div>
     <div class="header_center">
-        <span class="iconfont">&#xe632;</span>
-        输入城市/景点/游玩主题
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
     </div>
-    <div class="header_right">{{this.city}} <span class="iconfont">&#xe64a;</span></div>
-</div>
+    <router-link to="city">
+      <div class="header_right">
+        {{this.city}}
+        <span class="iconfont">&#xe64a;</span>
+      </div>
+    </router-link>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "HomeHeader",
-    props: {
-        city: String
-    }
+  name: "HomeHeader",
+  props: {
+    city: String
+  }
 };
 </script>
 
 <style lang="stylus" scoped>
 // @import "../../../assets/style/varibles"
 // @import "~@/assets/style/varibles"
-@import "~style/varibles"
+@import '~style/varibles';
 
 .header {
-    background-color: $bgColor;
-    overflow: hidden;
+  background-color: $bgColor;
+  overflow: hidden;
+  color: #fff;
+  padding: 0.2rem;
+
+  .header_left {
+    float: left;
+  }
+
+  .header_center {
+    float: left;
+    margin: 0 0.2rem;
+  }
+
+  .header_right {
+    float: right;
     color: #fff;
-    padding: 0.2rem;
-
-    .header_left {
-        float: left;
-    }
-
-    .header_center {
-        float: left;
-        margin: 0 0.2rem;
-    }
-
-    .header_right {
-        float: right;
-    }
+  }
 }
 </style>
