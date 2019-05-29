7949d33f2f06a92b17c9cf271ad3828262753c64
he30265 <18104860443@163.com>
2019-05-29 17:26:46 +0800

    vuex test

--- src/pages/city/components/list.vue
+++ src/pages/city/components/list.vue
@@ -5,14 +5,14 @@
             <div class="ls_tit">当前城市</div>
             <div class="ls_li">
                 <div class="button_box">
-                    <div class="button">北京</div>
+                    <div class="button">{{this.$store.state.city}}</div>
                 </div>
             </div>
         </div>
         <div class="lw_section">
             <div class="ls_tit">热门城市</div>
             <div class="ls_li">
-                <div class="button_box" v-for="item of hotCities" :key="item.id">
+                <div class="button_box" v-for="item of hotCities" :key="item.id" @click="handleCityClick(item.name)">
                     <div class="button">{{item.name}}</div>
                 </div>
             </div>
@@ -21,7 +21,7 @@
             <div v-for="(item,key) of cities" :key="key" :ref="key">
                 <div class="ls_tit">{{key}}</div>
                 <div class="ls_li">
-                    <div class="alp_li border-bottom" v-for="city of item" :key="item.id">{{city.name}}</div>
+                    <div class="alp_li border-bottom" v-for="city of item" :key="item.id" @click="handleCityClick(city.name)">{{city.name}}</div>
                 </div>
             </div>
         </div>
@@ -40,7 +40,6 @@ export default {
     },
     mounted() {
         this.scroll = new BScroll(this.$refs.wrapper);
-        console.log(this.scroll)
     },
     watch: {
         letter() {
@@ -49,6 +48,12 @@ export default {
                 console.log(this.$refs)
                 this.scroll.scrollToElement(element);
             }
+        }
+    },
+    methods :{
+        handleCityClick(city){
+            // this.$store.dispatch("changeCity",city);
+            this.$store.commit("changeCity",city);
         }
     }
 };
--- src/pages/city/components/search.vue
+++ src/pages/city/components/search.vue
@@ -1,12 +1,62 @@
 <template>
 <div class="search">
-    <input class="ipt" type="text" placeholder="输入城市名或拼音">
+    <input v-model="keyword" class="ipt" type="text" placeholder="输入城市名或拼音">
+    <div class="search-content" ref="content" v-show="keyword">
+        <ul>
+            <li v-for="item of list" :key="item.id">{{item.name}}</li>
+            <li v-show="noKeyword">无匹配项</li>
+        </ul>
+    </div>
 </div>
 </template>
 
 <script>
+import BScroll from "better-scroll"
 export default {
-    name: "CitySearch"
+    name: "CitySearch",
+    props: {
+        cities: Object
+    },
+    data() {
+        return {
+            keyword: "",
+            list: [],
+            timer: null
+        };
+    },
+    computed:{
+        noKeyword(){
+            return !this.list.length;
+        }
+    },
+    watch: {
+        keyword() {
+            if(this.keyword ==""){
+                this.list=[]
+                return
+            }
+            if (this.timer) {
+                clearTimeout(this.timer);
+            }
+            this.timer = setTimeout(() => {
+                const result = [];
+                for (let i in this.cities) {
+                    this.cities[i].forEach((value) => {
+                        if (
+                            value.name.indexOf(this.keyword) > -1 ||
+                            value.spell.indexOf(this.keyword) > -1
+                        ) {
+                            result.push(value);
+                        }
+                    });
+                }
+                this.list = result;
+            }, 100);
+        }
+    },
+    mounted() {
+        this.scroll = new BScroll(this.$refs.content);
+    },
 };
 </script>
 
@@ -16,21 +66,31 @@ export default {
 .search {
     background-color: $bgColor;
     overflow: hidden;
-    color: #fff;
-    padding: 0 .2rem;
-    height .9rem
-    line-height .9rem
+    padding: 0 0.2rem;
+    height: 0.9rem;
+    line-height: 0.9rem;
 
     .ipt {
         width: 100%;
         background-color: #fff;
         text-align: center;
         color: #666;
-        height: .5rem;
-        line-height: .5rem;
+        height: 0.5rem;
+        line-height: 0.5rem;
         box-sizing: border-box;
-        padding: 0 .2rem;
+        padding: 0 0.2rem;
+    }
+
+    .search-content {
+        position: absolute;
+        top: 1.62rem;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color: #f5f5f5;
+        z-index: 1;
+        padding: 0 0.2rem;
+        overflow: hidden;
     }
 }
-
 </style>
